89b691f3ef9d53fa2a415bdb0a59a0035b983149
itcha2 <twelvebarblues@hotmail.com>
2015-06-22 11:20:24 +1000

    Create new file
    Began work on fixing suspected issue associated with converting non-text byte values to as string and back;

---
+++ src/net/programmingpandas/firehug/ByteSearch.java
@@ -0,0 +1,21 @@
+
+public class ByteSearch {
+    
+    protected byte[] b = null;
+    
+    public ByteSearch(){
+        
+    }
+    
+    public ByteSearch(byte[] b){
+        this.b = b;
+    }
+    
+    public void set(byte[] b){
+        this.b = b;
+    }
+    
+    public byte[] get(){
+        return b;
+    }
+}
