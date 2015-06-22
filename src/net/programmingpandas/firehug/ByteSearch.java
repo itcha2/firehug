be966118b4f3f84fa7b2c25283baa1efe87f1141
itcha2 <twelvebarblues@hotmail.com>
2015-06-22 13:09:01 +1000

    Update ByteSearch.java
    finished array support thingy. can now integrate into firehug.

--- src/net/programmingpandas/firehug/ByteSearch.java
+++ src/net/programmingpandas/firehug/ByteSearch.java
@@ -18,4 +18,41 @@ public class ByteSearch {
     public byte[] get(){
         return b;
     }
+    
+    public void removePattern(byte[] pattern) {
+        
+        int y;
+        
+        if (b == null || !b.length > 0) {
+            return;
+        }
+        
+        biterate: for(int x = 0; x < b.length; x++){
+            if (b[x] == pattern[0]){
+                patterniterate: for (y = 0; y < pattern.length; y++){
+                    if(b[x+y] != pattern[y]){
+                        continue biterate;
+                    }
+                }
+              for (int z = 0; z < b.length - (x + y); z++) {
+                  b[x + z] = b[x + y + z];
+              }
+              for (int z = x + y; z < b.length; z++) {
+                  b[z] = null;
+              }
+            }
+        }
+    }
+    
+    public void appendPattern(byte[] pattern){
+        byte newb = new byte[b.length + pattern.length];
+        int x;
+        for(x = 0; x < b.length; x++){
+            newb[x] = b[x];
+        }
+        for (; x < b.length + pattern.length){
+            newb[x] = pattern[x - b.length];
+        }
+        b = newb;
+    }
 }
